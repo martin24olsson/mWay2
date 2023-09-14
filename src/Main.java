b44e6ae612898ed9bf59861da57eae78d0874cb7
malmmattias <malmmattias1@outlook.com>
2023-09-14 14:54:51 +0200

    hej

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,10 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello Martin!");
     }
-    //new
+    /*
+    För att kunna lägga ihop tal måste vi ha en loopfunktion där talet x är mindre än n (x<n);
+    Vi måste också kunna deklarera variabler och tilldela dessa värden.
+    Vi måste kunna addera variabler med varandra.
+    Vi måste kunna summera variabler.
+     */
 }
